c67652b676671c8d7b208ae93ddaf606ed9fbf50
saman <samanabid8715@gmail.com>
2023-09-17 10:29:29 +0530

    msg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { UserProvider } from './UserContext';
+import NewsLetter from './components/Newsletter';
 
 function App() {
   return (
@@ -11,8 +15,9 @@ function App() {
       <Navbar/>
         <Routes>
           <Route element={ <Home/>} path='/'/>
-          {/* <Route element={ <Login/>} path='/login'/> */}
-          {/* <Route element={ <Signup/>} path='/signup'/> */}
+          <Route element={ <Login/>} path='/login'/>
+          <Route element={ <Signup/>} path='/signup'/>
+          <Route element={ <NewsLetter/>} path='/news'/>
         </Routes>
       </UserProvider>
     </BrowserRouter>
