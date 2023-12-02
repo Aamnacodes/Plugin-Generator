94edcee0391317655f139bdad11c2799da4f06d0
saman <samanabid8715@gmail.com>
2023-12-03 01:04:41 +0530

    modifications

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShuffle, faPalette, faChartPie } from '@fortawesome/free-solid-svg-icons';
+import { faShuffle, faChartPie, faCommentDots } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
@@ -31,7 +31,46 @@ const Home = () => {
         </div>
       </main>
 
-      <div className='text-center temp' id='templates' style={{ padding: '80px',fontFamily: 'Roboto Slab, serif' }}>
+
+    <section className='temp' style={{marginTop:'-20px', fontFamily: 'Roboto Slab,serif'}} id='intro'>
+    <p className=' display-4 p-2 mb-4 shadow rounded-pill text-center fw-bold fst-italic'  style={{color:'#F6F4EB', backgroundColor:'#4E6E81',  marginTop:'-50px'}}>Why Choose Us?</p>
+    <div className="container py-4">
+    <div className="p-5 mb-3 rounded-3" style={{marginTop:'-10px', backgroundColor:'#B6BBC4'}}>
+    <h1 className="display-5 fw-bold">Elevate Your Communication Effortlessly</h1>
+    
+     <div className='d-flex'>
+     <div className="container-fluid " style={{width:'85%'}}>
+        <p className="p-3 fst-italic fs-5">In the bustling world of digital communication, our platform stands as a beacon of simplicity and efficiency. Welcome to MailMagnet - your trusted partner in newsletter convenience.
+        At MailMagnet, we understand the challenges of crafting and managing compelling newsletters. Our mission is clear: to empower businesses and individuals, enhancing their ability to connect with their audience, elevate communication strategies, and achieve their goals seamlessly.</p>
+      </div>
+      <div className="d-flex align-items-center" style={{ width: '15%' }} >
+          <img 
+          src='\letter-1174_256.gif'
+          width={150} height={150} 
+          alt='main' />
+      </div>
+      </div>
+
+    </div>
+
+    <div className="row align-items-md-stretch">
+      <div className="col-md-6">
+        <div className="h-100 p-5 text-light rounded-3" style={{backgroundColor:'#31304D'}}>
+          <h2>Effortless Creation and Management</h2>
+          <p className='fst-italic'>Gone are the days of intricate newsletter creation. With MailMagnet, you embark on a journey of effortless newsletter design and management. Our intuitive platform streamlines the entire process, ensuring that even those with no design or technical background can craft professional and visually stunning newsletters.</p>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <div className="h-100 p-5 border rounded-3" style={{backgroundColor:'#F0ECE5'}}>
+          <h2>Connect with Your Audience</h2>
+          <p className='fst-italic'>Connecting with your audience shouldn't be a cumbersome task. MailMagnet simplifies this crucial aspect, providing you with tools that foster meaningful engagement. Whether you're a business aiming to strengthen client relationships or an individual sharing your passion, our platform ensures that your messages resonate with impact.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+      {/* <div className='text-center temp' id='templates' style={{ padding: '80px',fontFamily: 'Roboto Slab, serif' }}>
         <p className='fw-bold px-5 display-6 fst-italic' style={{ color: '#053B50' }}>Select the Perfect Template</p>
         <div className='row mt-5 justify-content-between'>
           <div className="col-md-4 cards mb-4">
@@ -67,10 +106,10 @@ const Home = () => {
           </div>
           </div>
           </div>
-          </div>
+          </div> */}
 
-    <div className='px-5 feat border-top' id='features'  style={{ padding: '120px', fontFamily: 'Roboto Slab, serif' }}>
-      <p className='pb-2 display-5 mb-5 text-center fw-bold fst-italic'  style={{ color: '#053B50', marginTop:'-50px'}}>Features we provide</p>
+    <div className='px-5 feat' id='features'  style={{ padding: '120px', fontFamily: 'Roboto Slab, serif' }}>
+    <p className=' display-4 p-2 mb-4 shadow rounded-pill text-center fw-bold fst-italic'  style={{color:'#053B50', backgroundColor:'#D8D9DA', width:'70%', marginLeft:'190px', marginTop:'-50px'}}>Features we Provide</p>
       <div className='row g-4 py-5 row-cols-1 row-cols-lg-3'>
         <div className='feature col'>
           <div className='d-inline-flex align-items-center justify-content-center fs-1 mb-2'>
@@ -88,10 +127,10 @@ const Home = () => {
         </div>
         <div className='feature col'>
           <div className='d-inline-flex align-items-center justify-content-center fs-1 mb-2'>
-          <FontAwesomeIcon icon={faPalette} />
+          <FontAwesomeIcon icon={faCommentDots} />
           </div>
-          <h3 className='fs-3 fw-bold mt-3 mb-4' style={{color:'#053B50'}}>Customizable Templates</h3>
-          <p style={{fontSize:'0.9em', color:'#053B50', fontStyle:'italic'}}>"Create stunning newsletters that match your brand's identity with our customizable templates. Choose from a wide range of professionally designed templates or personalize your own with our easy-to-use editor. Tailor your newsletters to your audience, giving you the perfect blend of style and substance to captivate your readers."</p>
+          <h3 className='fs-3 fw-bold mt-3 mb-4' style={{color:'#053B50'}}>Improved Communication</h3>
+          <p style={{fontSize:'0.9em', color:'#053B50', fontStyle:'italic'}}>"Communication lies at the heart of success. Connecting with your audience shouldn't be a cumbersome task. MailMagnet is designed to enhance your communication strategies, enabling you to convey your message with clarity and style. Say goodbye to convoluted processes and hello to a platform that prioritizes seamless and effective communication."</p>
         </div>
       </div>
     </div>
@@ -110,7 +149,7 @@ const Home = () => {
         <h5 className="mb-4 fw-bold text-decoration-underline">Quick Links</h5>
         <ul className="list-unstyled social-icon-cont">
           <li className='socialicon'><a className='socialicon' href="#">Home</a></li>
-          <li className='socialicon'><a className='socialicon' href="#templates">Templates</a></li>
+          <li className='socialicon'><a className='socialicon' href="#intro">Why us?</a></li>
           <li className='socialicon'><a className='socialicon' href="#features">Features</a></li>
         </ul>
       </div>
