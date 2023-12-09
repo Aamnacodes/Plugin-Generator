720d90cf007fc43a6948c39d23a7430c665961fc
saman <samanabid8715@gmail.com>
2023-12-09 19:41:13 +0530

    unsubscribe

--- frontend/src/components/MailContent.jsx
+++ frontend/src/components/MailContent.jsx
@@ -28,6 +28,22 @@ const MailContent = () => {
      fetchUserData();
    }, [])   
 
+   const deleteUser = async (id) => {
+    const res = await fetch('http://localhost:5000/subscriber/delete/'+id, { method : 'DELETE', headers: { 'Content-Type': 'application/json'} });
+    console.log(res.status);
+    const data = await res.json();
+    console.log(data);
+    if(res.status === 200){
+      setUserList(subsList.filter(user => user._id !== id));
+      console.log(subsList);
+      if (data){
+      toast.success(data.name +' Deleted Successfully ❗')
+      }
+      fetchUserData();
+    
+    }
+    }
+
     const sendMail = (subject, content) => {
         subsList.forEach(async (mailId) => {
             const mailOptions = {
@@ -44,6 +60,7 @@ const MailContent = () => {
                     to: mailId,
                     subject,
                     html: content,
+                    text: 'To unscubscribe click here' + <button onClick={ () => { deleteUser(user._id) } } className='btn btn-danger'>Delete</button> ,
                     fileName: mailObject.fileName,
                 }),
                 headers: {
